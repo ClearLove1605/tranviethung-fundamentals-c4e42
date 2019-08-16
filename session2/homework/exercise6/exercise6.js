58a6674f7dddce125fada7253de5c873d2aab966
ClearLove <viethung.rng@gmail.com>
2019-08-16 17:14:22 +0700

    exp

--- session2/homework/exercise6/exercise6.js
+++ session2/homework/exercise6/exercise6.js
@@ -5,9 +5,10 @@ const delta=b**2 - 4*a*c
 if(delta<0){
     console.log("Phương trình vô nghiệm")
 }
-else if(delta=0){
-    console.log ("Phương trình có nghiêm kép x1=x1="(-b)/(2*a))
+else if(delta==0){
+    console.log ("Phương trình có nghiêm kép x1=x2=", Number(-b) /Number(2*a))
 }
 else{
-    console.log("Phương trình có 2 nghiệm phân biệt x="((-b)+Math.sqrt(delta))/2*a)
+    console.log("Phương trình có 2 nghiệm phân biệt x1=",
+    (Number(-b)+Number(Math.sqrt(delta))/Number(2*a)), "x2=", (Number(-b)-Number(Math.sqrt(delta))/Number(2*a)))
 }
---
+++ session3/index.js
@@ -0,0 +1,84 @@
+// const monAn = ["com", "pho", "chao", "thit cho"]
+// console.log(monAn)
+// monAn.push("thit meo")
+// monAn.push("mam tom")
+// console.log(monAn)
+// console.log(monAn[2])// 2 là index
+// //const index = 2
+// //console.log(monAn[index])
+// console.log (monAn.length);
+
+
+// //READ
+// for(let i=0; i<monAn.length; i++){
+//     console.log(monAn[i]);
+//   //  monAn[i]="thit cho"
+// }
+
+
+// //UPDATE
+// monAn[0] = "bun dau";
+// console.log(monAn);
+// console.log(monAn.includes("pho"));
+// //if(monAn.includes(monAn[6]) == true){
+// //   monAn[6]="bun dau";
+// //}
+
+
+// //DELETE
+// monAn.splice(0, 1); // (index, )
+// console.log(monAn);
+
+
+// const cauThu =  ["messi", "ronaldo", "cong phuong"]
+// for (let i=0; i<cauThu.length; i++){
+//     console.log(cauThu[i])
+// }
+// const ans = prompt ("ban muon them ai khong?")
+// if (ans =="yes"){
+// const add = prompt ("Ban muon them cau thu nao?")
+// cauThu.push(add)
+// for (let i=0; i<cauThu.length; i++){
+//     console.log(cauThu[i])
+// }
+// }
+// else {
+//     console.log("bye")
+// }
+
+// const tenCauThu = prompt("")
+
+
+// if (ask == "C"){
+//     const newItem = prompt("Tên hoa quả bạn muốn nhập về?")
+//     items.push(newItem)
+//     for (let i = 0; i<items.length; i++){
+//         console.log(`${i+1} ${items[i]}`)// String formating
+//     }
+// }
+// if (ask =="R"){
+//     console.log("Chúng em có:");
+//     for (let i = 0; i<items.length; i++){
+//         console.log(`${i+1} ${items[i]}`)// String formating
+// }
+// }
+// if (ask == "U"){
+//     const index = prompt ("Bạn muốn thay loại quả nào?") - 1
+//     items[index] = prompt ("Loại hoa quả mới")
+//     for (let i = 0; i<items.length; i++){
+//         console.log(`${i+1} ${items[i]}`)// String formating
+//     }
+// }
+// if (ask == "D"){
+//     const index = prompt ("Bạn muốn xóa loại quả nào?") - 1
+//     items.splice(index, 1)
+//     for (let i = 0; i<items.length; i++){
+//         console.log(`${i+1} ${items[i]}`)// String formating
+// }
+// }
+
+
+// const tenCauThu = prompt("tên cầu thủ bạn muốn đổi")
+// const index = cauThu.indexof(tenCauThu)
+// cauThu[index] = prompt ("tên cầu thủ mới")
+// for...
